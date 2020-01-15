1b9479c449a40f0a7b726a6c29aa737419b4675e
lisa benato <lisa.benato@cern.ch>
2020-01-15 14:04:39 +0100

    Additional root files for testing

--- python/Ntuplizer2018.py
+++ python/Ntuplizer2018.py
@@ -124,7 +124,7 @@ process.options.numberOfThreads=cms.untracked.uint32(8)
 process.options.numberOfStreams=cms.untracked.uint32(0)
 
 ## Events to process
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(500) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 ## Messagge logger
 process.load("FWCore.MessageService.MessageLogger_cfi")
