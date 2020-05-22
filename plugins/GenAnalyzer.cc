f0a7a86bac9ea014c34e2f068e6eb3cc35584f66
lisa benato <lisa.benato@cern.ch>
2020-05-22 15:22:48 +0200

    Added method to deal with two LLPs pdgIds (happening for SUSY samples)

--- plugins/GenAnalyzer.cc
+++ plugins/GenAnalyzer.cc
@@ -167,6 +167,22 @@ std::vector<reco::GenParticle> GenAnalyzer::FillGenVectorByIdAndStatus(const edm
     return Vect;
 }
 
+std::vector<reco::GenParticle> GenAnalyzer::FillGenVectorByTwoIdsAndStatus(const edm::Event& iEvent, int partid1, int partid2, int partstatus) {
+
+    std::vector<reco::GenParticle> Vect;
+
+    // check if is real data
+    isRealData = iEvent.isRealData();
+    if(isRealData or PythiaLOSample) return Vect;
+    // fill collection for this event 
+    iEvent.getByToken(GenParticlesToken, GenCollection);
+    // Loop on Gen Particles collection
+    for(std::vector<reco::GenParticle>::const_iterator it = GenCollection->begin(); it != GenCollection->end(); ++it) {
+            if( (abs(it->pdgId()) == partid1 || abs(it->pdgId()) == partid2 ) && (it->status()) == partstatus) Vect.push_back(*it); // Fill vector
+    }
+    return Vect;
+}
+
 std::vector<reco::GenParticle> GenAnalyzer::FillGenVectorByIdListAndStatusAndMotherList(const edm::Event& iEvent, std::vector<int> partidlist, int partstatus, std::vector<int> motheridlist) {
 
     std::vector<reco::GenParticle> Vect;
@@ -183,7 +199,7 @@ std::vector<reco::GenParticle> GenAnalyzer::FillGenVectorByIdListAndStatusAndMot
 
 	for(unsigned int m = 0; m < motheridlist.size(); m++) {
 
-	    if(abs(it->pdgId()) == s && (it->status()) == partstatus && abs(it->mother()->pdgId()) == m) Vect.push_back(*it); // Fill vector
+	    if(abs(it->pdgId()) == partidlist[s] && (it->status()) == partstatus && abs(it->mother()->pdgId()) == m) Vect.push_back(*it); // Fill vector
 
 	}
 
