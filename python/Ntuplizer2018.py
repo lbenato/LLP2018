e1fc71fd1bce4b72bc6fdb9987a6ada7f92df93d
lisa benato <lisa.benato@cern.ch>
2021-01-25 16:01:22 +0100

    -1 max events

--- python/Ntuplizer2018.py
+++ python/Ntuplizer2018.py
@@ -292,7 +292,7 @@ process.options.numberOfThreads=cms.untracked.uint32(8)
 process.options.numberOfStreams=cms.untracked.uint32(0)
 
 ## Events to process
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10000) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 ## Messagge logger
 process.load("FWCore.MessageService.MessageLogger_cfi")
